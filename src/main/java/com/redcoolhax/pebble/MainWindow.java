fcf048fbc87db9c0179c78abf901587e484e428c
Redcoolhax@github.com <185998324+Redcoolhax@users.noreply.github.com>
2025-08-20 15:29:26 -0400

    Added a new send request button accompanied by a respective ActionListener method, which sends the info from the request method, uri field, and request body. This was tested with a localhost http server and seems to function properly.

--- src/main/java/com/redcoolhax/pebble/MainWindow.java
+++ src/main/java/com/redcoolhax/pebble/MainWindow.java
@@ -1,10 +1,17 @@
 package com.redcoolhax.pebble;
 
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import java.awt.Point;
 
 public class MainWindow extends JFrame {
     private static final int REQUEST_LINE_INPUT_HEIGHT = 30;
@@ -13,6 +20,7 @@ public class MainWindow extends JFrame {
     private JTextField uriField;
     private JTextField httpVersionField;
     private JTextArea requestBody;
+    private JButton sendRequestButton;
 
     public MainWindow() {
         super("Pebble");
@@ -56,6 +64,28 @@ public class MainWindow extends JFrame {
         requestBody.setBounds(50, 100, 300, 200);
         add(requestBody);
 
+        sendRequestButton = new JButton("Send Request");
+        sendRequestButton.setBounds(400, 300, 100, 40);
+        sendRequestButton.addActionListener(this::onSendRequestButtonPress);
+        add(sendRequestButton);
+
         setVisible(true);
+    }
+
+    private void onSendRequestButtonPress(ActionEvent event) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(uriField.getText()))
+            .method(
+                requestMethodSelection.getSelectedItem().toString(),
+                HttpRequest.BodyPublishers.ofString(requestBody.getText())
+            )
+            .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
