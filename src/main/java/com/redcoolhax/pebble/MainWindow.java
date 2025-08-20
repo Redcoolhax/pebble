aa674960b51d94439e7a0f20a5d622b69ac005f5
Redcoolhax@github.com <185998324+Redcoolhax@users.noreply.github.com>
2025-08-20 13:45:03 -0400

    Restructured the code in MainWindow's constructor to better organize the components. Also added a new field for specifying an HTTP version.

--- src/main/java/com/redcoolhax/pebble/MainWindow.java
+++ src/main/java/com/redcoolhax/pebble/MainWindow.java
@@ -4,17 +4,23 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.awt.Point;
 
 public class MainWindow extends JFrame {
+    private static final int REQUEST_LINE_INPUT_HEIGHT = 30;
+
     private JComboBox<String> requestMethodSelection;
     private JTextField uriField;
+    private JTextField httpVersionField;
     private JTextArea requestBody;
 
     public MainWindow() {
         super("Pebble");
-        setSize(400, 400);
+        setSize(600, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
+
+        Point topLeft = new Point(10, 10);
 
         String[] requestMethods = {
             "GET",
@@ -28,12 +34,23 @@ public class MainWindow extends JFrame {
             "TRACE"
         };
         requestMethodSelection = new JComboBox<>(requestMethods);
-        requestMethodSelection.setBounds(10, 10, 150, 30);
+        requestMethodSelection.setLocation(topLeft);
+        requestMethodSelection.setSize(100, REQUEST_LINE_INPUT_HEIGHT);
         add(requestMethodSelection);
 
+        topLeft.x += requestMethodSelection.getWidth() + 10;
+
         uriField = new JTextField();
-        uriField.setBounds(10, 50, 150, 30);
+        uriField.setLocation(topLeft);
+        uriField.setSize(300, REQUEST_LINE_INPUT_HEIGHT);
         add(uriField);
+
+        topLeft.x += uriField.getWidth() + 10;
+
+        httpVersionField = new JTextField();
+        httpVersionField.setLocation(topLeft);
+        httpVersionField.setSize(100, REQUEST_LINE_INPUT_HEIGHT);
+        add(httpVersionField);
 
         requestBody = new JTextArea();
         requestBody.setBounds(50, 100, 300, 200);
