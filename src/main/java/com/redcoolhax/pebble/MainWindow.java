acd2b7b763a37b2eae647a343117aca5a3b88fa0
Redcoolhax@github.com <185998324+Redcoolhax@users.noreply.github.com>
2025-08-20 11:19:25 -0400

    Added a JComboBox for HTTP request methods.

--- src/main/java/com/redcoolhax/pebble/MainWindow.java
+++ src/main/java/com/redcoolhax/pebble/MainWindow.java
@@ -1,12 +1,32 @@
 package com.redcoolhax.pebble;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame {
+    private JComboBox<String> requestMethodSelection;
+
     public MainWindow() {
         super("Pebble");
         setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+
+        String[] requestMethods = {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "PATCH",
+            "HEAD",
+            "OPTIONS",
+            "CONNECT",
+            "TRACE"
+        };
+        requestMethodSelection = new JComboBox<>(requestMethods);
+        requestMethodSelection.setBounds(50, 50, 150, 30);
+        add(requestMethodSelection);
+
         setVisible(true);
     }
 }
