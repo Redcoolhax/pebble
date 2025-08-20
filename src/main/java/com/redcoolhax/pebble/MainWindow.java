e4706c674908a8c837f0be28d7bf03411e328ea5
Redcoolhax@github.com <185998324+Redcoolhax@users.noreply.github.com>
2025-08-20 00:26:19 -0400

    Added the exec plugin to pom.xml, also set up a basic Swing window.

---
+++ src/main/java/com/redcoolhax/pebble/MainWindow.java
@@ -0,0 +1,12 @@
+package com.redcoolhax.pebble;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super("Pebble");
+        setSize(400, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
